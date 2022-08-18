97c6b7d7a95e7ca60c09705b4e53633ff544bc60
Grégoire HUBERT <hubert.greg@gmail.com>
2022-08-18 23:12:46 +0200

    enhance integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,20 @@
-use mars_rover::{OrderParserIterator, Robot};
+use mars_rover::{Order, OrderParserIterator, Robot};
 
 #[test]
 fn test_iterator() {
     let orders = "F FRFqLF";
     let mut robot = Robot::default();
 
-    for order in OrderParserIterator::new(&orders).filter_map(|v| v.ok()) {
+    for (index, order) in OrderParserIterator::new(&orders)
+        .filter_map(|v| v.ok())
+        .enumerate()
+    {
+        match index {
+            0 | 1 | 3 | 5 => assert_eq!(Order::MoveForward, order),
+            2 => assert_eq!(Order::TurnRight, order),
+            4 => assert_eq!(Order::TurnLeft, order),
+            _ => panic!("unexpected order '{}' at position {}", order, index),
+        }
         robot.please_do(order);
     }
 
